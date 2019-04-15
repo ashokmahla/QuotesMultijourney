69289736628367b8231be1104515cede93f44afc
Ashok Kumar <ashok.kumar@elephant.com>
2019-04-15 16:46:45 +0530

    [#buttonComponent] rrbuttonComponent: Inital button componet as a library.

---
+++ projects/button/src/lib/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ele-button',
+  template: `
+    <p>
+      Inital button testing
+    </p>
+  `,
+  styles: []
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
