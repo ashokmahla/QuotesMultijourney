ca8ebb5af16911db9a45b4178defbd3f722026b6
ashok.kumar <ashok@reverselogix.com>
2020-02-19 18:07:35 +0530

    Button delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'button'
+ 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ButtonModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
